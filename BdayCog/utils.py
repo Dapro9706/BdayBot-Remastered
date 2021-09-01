1089f84d7a34be09a64e55ed58a342a7e459dfd5
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-09-01 07:18:10 +0530

    Update utils.py

--- BdayCog/utils.py
+++ BdayCog/utils.py
@@ -1,5 +1,5 @@
 from discord import Embed, Colour
-
+from os import environ
 
 async def embed(ctx, content):
     await ctx.send (embed=Embed (description=content))
@@ -8,3 +8,20 @@ async def embed(ctx, content):
 async def birthday_embed(ctx, head, content, footer="xD"):
     await ctx.send (embed=Embed (title=":birthday: "+head+" :partying_face:", description=content, color=Colour.random ()).set_footer (
         text=f"btw its also {footer}'s birthday"))
+
+DATABASE_URL = environ['DATABASE_URL']
+
+def create_table():
+  c = psycopg2.connect(DATABASE_URL, sslmode='require')#sqlite3.connect(DATABASE)
+  c.cursor().execute('''CREATE TABLE LastCheck
+                      (ID INTEGER PRIMARY KEY,
+                       LC INTEGER)''')
+  con.cursor().execute(f"INSERT INTO Accounts VALUES (1, 21)")
+  c.commit()
+  c.close()
+
+def edit(a):
+  c = psycopg2.connect(DATABASE_URL, sslmode='require')#sqlite3.connect(DATABASE)
+  con.cursor().execute(f"UPDATE Accounts SET AMOUNT = {a} WHERE ID = 1")
+  c.commit()
+  c.close()
